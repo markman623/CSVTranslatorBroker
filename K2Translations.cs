5cf85bd2a9cbc340b93ca28dedf697c1e4fb8059
markw <markw@k2.com>
2024-10-11 22:10:51 +0200

    Initial comit

---
+++ K2Translations.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SourceCode.SmartObjects.Services.ServiceSDK;
+using SourceCode.SmartObjects.Services.ServiceSDK.Objects;
+using Attr = SourceCode.SmartObjects.Services.ServiceSDK.Attributes;
+using SourceCode.SmartObjects.Services.ServiceSDK.Types;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace K2Translations
+{
+    [Attr.ServiceObject("K2Translations", "K2Translations", "Used for translating terms and eventually CSVs in K2")]
+    public class K2Translations
+    {
+        // Service Object Properties
+        private string culture;
+        private string term;
+        private string translation;
+
+        [Attr.Property("Culture", SoType.Text, "Culture", "Culture to translate to, such as en-US")]
+        public string Culture { get { return culture; } set { culture = value; } }
+
+        [Attr.Property("Term", SoType.Text, "Term", "What to translate")]
+        public string Term { get { return term; } set { term = value; } }
+
+        [Attr.Property("Translation", SoType.Text, "Translation", "The translated term")]
+        public string Translation { get { return translation; } set { translation = value; } }
+
+        // Service Object Methods
+        [Attr.Method("TranslateTerm", MethodType.Read, "TranslateTerm", "Translate a term", 
+            new string[]{"Culture", "Term"}, 
+            new string[] {"Culture", "Term" },
+            new string[] {"Translation"})]
+        public K2Translations TranslateTerm() 
+        {
+            K2Translations k2Translations = new K2Translations();
+            k2Translations.translation = TranslateTerm(Term, Culture).Result;
+
+            return k2Translations;
+
+        }
+
+        private async Task<string> TranslateTerm(string term, string culture)
+        {
+            var requestUrl = ServiceConfiguration["ServiceURL"] + $"/translate?api-version=3.0&to={culture}";
+            var body = new object[] { new { Text = term } };
+            var requestBody = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+               {
+                Method = HttpMethod.Post,
+                Content = requestBody,
+                RequestUri = new Uri( requestUrl)
+            };
+
+            request.Headers.Add("Ocp-Apim-Subscription-Key", ServiceConfiguration["APIKey"].ToString());
+            request.Headers.Add("Ocp-Apim-Subscription-Region", ServiceConfiguration["Region"].ToString());
+
+            var response = await client.SendAsync(request);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var result =JArray.Parse(responseBody);
+
+            return result[0]["translations"][0]["text"].ToString();
+        }
+
+        // Adding a service Config property, so we can access it as needed
+        private ServiceConfiguration serviceConfiguration;
+
+        public ServiceConfiguration ServiceConfiguration
+        {
+            get { return serviceConfiguration; }
+            set { serviceConfiguration = value; }
+        }
+    }
+}
---
+++ K2TranslationsServiceBroker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SourceCode.SmartObjects.Services.ServiceSDK;
+using SourceCode.SmartObjects.Services.ServiceSDK.Objects;
+using SourceCode.SmartObjects.Services.ServiceSDK.Attributes;
+using SourceCode.SmartObjects.Services.ServiceSDK.Types;
+
+namespace K2Translations
+{
+    public class K2TranslationsServiceBroker : ServiceAssemblyBase
+    {
+        public override string GetConfigSection()
+        {
+            try
+            {
+                this.Service.ServiceConfiguration.Add("APIKey", true, "Need a key");
+                this.Service.ServiceConfiguration.Add("ServiceURL", true, "https://api.cognitive.microsofttranslator.com/");
+                this.Service.ServiceConfiguration.Add("Region", false, "eastus");
+            }
+            catch (Exception ex){
+                ServicePackage.ServiceMessages.Add(ex.Message, MessageSeverity.Error);
+            }
+            return base.GetConfigSection();
+        }
+
+        public override string DescribeSchema()
+        {
+            try
+            {
+                this.Service.ServiceObjects.Create(new ServiceObject(typeof(K2Translations)));
+                this.Service.Name = "K2Translator";
+                this.Service.MetaData.DisplayName = "K2Translator";
+                this.Service.MetaData.Description = "A service to translate terms, and eventually K2 translation CSVSs.";
+            }
+            catch (Exception ex) 
+            {
+                ServicePackage.ServiceMessages.Add(ex.Message, MessageSeverity.Error);
+                ServicePackage.IsSuccessful = false;
+            }
+
+            return base.DescribeSchema();
+        }
+
+        public override void Extend()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
